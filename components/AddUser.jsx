7cc57c00ab657524d031a1ed3f6a79d70b7afe34
Rowan Tate <rowan.tate@kitomba.com>
2019-11-14 14:57:43 +1300

    Added some sweet material design

--- components/AddUser.jsx
+++ components/AddUser.jsx
@@ -3,7 +3,7 @@ class AddUser extends React.Component {
         super(props);
         this.state = {
             name: "",
-            weight: 0,
+            weight: "",
             users: []
         };
 
@@ -17,7 +17,7 @@ class AddUser extends React.Component {
         db.collection("users")
           .onSnapshot((querySnapshot) => {
               let users = [];
-              querySnapshot.forEach(function(doc) {
+              querySnapshot.forEach(function (doc) {
                   users.push(doc.data());
               });
               console.log(users);
@@ -25,12 +25,14 @@ class AddUser extends React.Component {
           });
     }
 
-    addUser(){
+    addUser() {
         db.collection("users").add({
             name: this.state.name,
             weight: this.state.weight
         }).then(docRef => console.log("Document written with ID: ", docRef.id))
-          .catch(error => { console.error("Error adding document: ", error); });
+          .catch(error => {
+              console.error("Error adding document: ", error);
+          });
     }
 
     handleChange(event) {
@@ -42,29 +44,37 @@ class AddUser extends React.Component {
     }
 
 
-
     render() {
         return (
           <form onSubmit={(event) => event.preventDefault()}>
-              <label>
-                  Name
-                  <input id="name" value={this.state.name} onChange={this.handleChange} style={{"margin-left":"10px"}}/>
-              </label>
 
-              <br />
-              <div style={{"margin-top":"5px"}}></div>
-              <label>
-                  Weight
-                  <input value={this.state.weight} onChange={this.handleChangeWeight} style={{"margin-left":"10px"}}/>
-              </label>
+              <TextField
+                margin="dense"
+                variant="outlined"
+                id="name"
+                label="Name"
+                value={this.state.name}
+                onChange={this.handleChange} />
 
-              <br />
 
-              <button onClick={() => this.addUser()}>
+              <br/>
+
+              <TextField
+                margin="dense"
+                variant="outlined"
+                label="Weight"
+                value={this.state.weight}
+                onChange={this.handleChangeWeight}
+                /*style={{"marginLeft":"10px"}}*//>
+
+
+              <br/>
+
+              <Button variant="contained" color="primary" onClick={() => this.addUser()}>
                   Add
-              </button>
+              </Button>
 
-              <hr />
+              <hr/>
 
               <div>
                   {this.state.users.map((item) => (
